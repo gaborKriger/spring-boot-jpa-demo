a2a3217562444a8862cbcb1d27bf4144cdfd4aa7
Kriger Gábor <regirck@gmail.com>
2019-03-18 18:24:21 +0100

    create repository

---
+++ src/main/java/com/codecool/springdemojpademo/SpringDemoJpaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.springdemojpademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoJpaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoJpaDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/springdemojpademo/SpringDemoJpaDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codecool.springdemojpademo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDemoJpaDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
